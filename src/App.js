77b853d95767150b772f5d3ab1f0daa6c3227ca9
Khuong Truong <khuong.truong@deliveree.com>
2023-02-27 15:37:08 +0700

    add bulma to handle css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css'
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Aigital Assistance </p>
+                </div>
+            </section>
+            <div className="container">
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column'>
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@Alexa"
+                                src={AlexaImage}
+                                description="A new site for Alexa, Amazon's cloud based AI"
+                            />
+                        </div>
+                        <div className='column'>
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@Cortana"
+                                src={CortanaImage}
+                                description="Cortana is a virtual assistant developed by Microsoft that uses the Bing search engine to perform tasks such as setting reminders and answering questions for the user"
+                            />
+                        </div>
+                        <div className='column'>
+                            <ProfileCard
+                                title="Siri"
+                                handle="@Siri"
+                                src={SiriImage}
+                                description="Siri is a virtual assistant that is part of Apple Inc.'s iOS, iPadOS, watchOS, macOS, tvOS, and audioOS operating systems."
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/ProfileCard.js
@@ -0,0 +1,22 @@
+function ProfileCard({title, handle, src, description}) {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src = {src} alt="image bro" />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">
+                    <p>{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProfileCard;
