f63a3a8b4380e2b597f56ead97581d1b88d16dc1
Khuong Truong <khuong.truong@deliveree.com>
2023-03-07 22:20:53 +0700

    feature/apply-api-for-search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
-import { useState } from "react";
-import AnimalShow from "./AnimalShow";
-import './App.css'
-// app support animals
-function getRandomAnimal() {
-    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
-    return animals[Math.floor(Math.random() * animals.length)]
-}
-
 function App() {
-    const [animals, setAnimals] = useState([])
-    const handleClick = () => {
-        setAnimals([...animals, getRandomAnimal()]);
-    };
-    const renderedAnimals = animals.map((animal, index) => {
-        return <AnimalShow type={animal} key={index} />;
-    });
-    return (
-        <div className="app">
-            <button onClick={handleClick}>Add Animal</button>
-            <div className="animal-list">{renderedAnimals}</div>
-        </div>
-    );
+    
 }
 export default App;
