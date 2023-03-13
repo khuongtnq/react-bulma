8748c3bce083b9bcff7ce6e7d177c4558e4c976f
Khuong Truong <khuong.truong@deliveree.com>
2023-03-13 14:27:54 +0700

    fix comment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import React from "react";
 import ReactDOM  from "react-dom/client";
 import searchImages from "./api";
 
-searchImages();
+// call api from unsplash
+searchImages(); 
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
